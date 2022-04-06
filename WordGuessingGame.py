36641f1115cd3e727e117010ce4e9c86fbeb1db5
Parth J Patel <pateljparth10@gmail.com>
2022-04-06 14:14:16 -0400

    second commit

--- WordGuessingGame.py
+++ WordGuessingGame.py
@@ -1,14 +1,43 @@
 word_guess= "Mississippi"
 word_split= word_guess.split()
-word_list = []
+guess_list = []
 blank_word = []
 guesses = 7
-print(len(word_guess))
+
 for i in range(len(word_guess)):
     blank_word.append("_")
-print(blank_word)
+  
+def check_guess(guess, guesses):
+    if(guess == guess_list):
+        guesses -=1
+        print("You have already guesses that. You have " +str(guesses) + " remaining. You have already guesses " + str(guess_list))
+    if(len(guess) == 1):
+        if(word_guess.find(guess)== -1):
+            guess_list.append(guess)
+            guesses -=1
+            print(guess + " was not in the word. You have " + str(guesses) + " Try again!")
+        else:
+            for position,letter in enumerate(word_guess):
+                if (letter == guess):
+                    blank_word[position] = guess
+            print(blank_word)
+    else:
+        guess_list.append(guess)
+        print("That is not a valid guess. Please try again.")
 
-for position,letter in enumerate(word_guess):
-    if (letter == "i"):
-       blank_word[position] = "i"
-print(blank_word)
+    if(len(guess) == len(word_guess)):
+        if (guess == word_guess):
+            print("Congratgulation! You guessed the word correctly!")
+        else:
+            guess_list.append(guess)
+            guesses -= 1
+            print(guess + " was not correct. You have " +str(guesses)+ " remaining. Try again!")
+    else:
+        guess_list.append(guess)
+        print("That is not a valid guess. Guess is either too long or too short. Please try again.")
+            
+check_guess("bob", guesses)  
+
+
+# while guesses > 0:
+#     guess = str(input("Enter a letter or word to guess the correct word."))
